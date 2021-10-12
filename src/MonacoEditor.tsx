2219a1417acf036b051c3b249beba97d8b342357
Tomohisa Igarashi <tm.igarashi@gmail.com>
2021-10-12 10:42:48 -0400

    not referring to esm module directly (someone said jest doesn't like esm)

--- src/MonacoEditor.tsx
+++ src/MonacoEditor.tsx
@@ -1,11 +1,11 @@
-import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+import { editor } from "monaco-editor";
 import { FunctionComponent, useEffect } from "react";
 import React from "react";
 
 export const MonacoEditor: FunctionComponent = () => {
   
   useEffect(() => {
-    monaco.editor.create(document.getElementById("monaco-editor")!, {
+    editor.create(document.getElementById("monaco-editor")!, {
       value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
       language: 'javascript'
     });
